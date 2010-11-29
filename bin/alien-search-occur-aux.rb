23f4648f03f2e00d11af77ffc049a8719e9b05c1
k-taro <Keitaro.Miyazaki@gmail.com>
2010-11-30 04:34:56 +0900

    Added ruby scripts.

---
+++ bin/alien-search-occur-aux.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8-unix -*-
+
+abort "Ruby version is too old (1.9 or later is required)." if RUBY_VERSION < "1.9"
+
+def main ()
+  fn_in, fn_out, fn_pat = ARGV
+  
+  str_pat = open(fn_pat, 'r:UTF-8') {|f| f.read}
+  offset = 0
+  
+  $stdout = open(fn_out, 'w:UTF-8')
+  
+  print "(setq result '("
+  
+  open(fn_in, 'r:UTF-8') do |file_in|
+    while line = file_in.gets do
+      matched = 0
+      len = line.length;
+      
+      line.scan( Regexp.new(str_pat) ) do
+        print '(' if matched == 0
+        print '('
+        print offset + Regexp.last_match.begin(0), ' '
+        print offset + Regexp.last_match.end(0)
+        print ')'
+        matched += 1;
+      end
+      print ')' if matched != 0
+      
+      offset += len
+    end
+  end
+  
+  print "))\n";
+  print ";;; EOF\n";
+  
+  exit 0
+  
+rescue RegexpError
+  $stderr.print $!.message
+  exit 1
+end
+
+main
+
+# EOF
---
+++ bin/alien-search-replace-aux.rb
@@ -0,0 +1,51 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8-unix -*-
+
+abort "Ruby version is too old (1.9 or later is required)." if RUBY_VERSION < "1.9"
+
+def escape_str_to_eval! (str)
+  str.gsub!(/"/ ){'\\"'}
+end
+
+def escape_ruby_str_for_emacs! (str)
+  str.gsub!(/\\/) {'\\\\'}
+  str.gsub!(/"/ ) {'\\"'}
+end
+
+def main ()
+  fn_in, fn_out, fn_pat, fn_rpl = ARGV
+  
+  str_in  = open(fn_in,  'r:UTF-8') {|f| f.read}
+  str_pat = open(fn_pat, 'r:UTF-8') {|f| f.read}
+  str_rpl = open(fn_rpl, 'r:UTF-8') {|f| f.read}
+  
+  escape_str_to_eval!(str_rpl)
+  
+  $stdout = open(fn_out, 'w:UTF-8')
+  
+  print "(setq result '("
+  
+  str_in.scan( Regexp.new(str_pat) ) do |m|
+    replacement = eval '"' + str_rpl + '"'
+    escape_ruby_str_for_emacs!(replacement)
+    
+    print '('
+    print Regexp.last_match.begin(0), ' '
+    print Regexp.last_match.end(0),   ' '
+    print '"', replacement, '"'
+    print ')'
+  end
+  
+  print "))\n";
+  print ";;; EOF\n";
+  
+  exit 0
+  
+rescue RegexpError
+  $stderr.print $!.message
+  exit 1
+end
+
+main
+
+# EOF
